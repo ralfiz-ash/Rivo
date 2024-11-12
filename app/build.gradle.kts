9647a6b96696d7b15cb9591ca484003a6ebe20e9
Ridill <ridillperson+github@gmail.com>
2024-11-12 23:58:55 +0530

    Fix incorrect version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,14 +54,14 @@ android {
         create("internal") {
             dimension = "env"
             applicationIdSuffix = ".internal"
-            versionCode = 1
-            versionName = "0.0.1"
+            versionCode = 2
+            versionName = "0.0.2"
         }
 
         create("production") {
             dimension = "env"
-            versionCode = 2
-            versionName = "0.0.2"
+            versionCode = 1
+            versionName = "0.0.1"
         }
     }
 
