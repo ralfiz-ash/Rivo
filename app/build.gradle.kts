8b05b405dfa55e040919e580dd40e061f2504a94
Ridill <ridillperson+github@gmail.com>
2025-01-16 20:01:49 +0530

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         create("internal") {
             dimension = "env"
             applicationIdSuffix = ".internal"
-            versionCode = 5
-            versionName = "0.0.5"
+            versionCode = 6
+            versionName = "0.0.6"
         }
 
         create("production") {
